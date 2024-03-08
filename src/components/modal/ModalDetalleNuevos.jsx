d8799487f7e34c62b0fa7f7fbd254cc6cfd17601
Nayelly <nayellypantoja2000@gmail.com>
2024-03-08 17:01:15 -0500

    detalle de asistentes

--- src/components/modal/ModalDetalleNuevos.jsx
+++ src/components/modal/ModalDetalleNuevos.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { Box, Modal } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
